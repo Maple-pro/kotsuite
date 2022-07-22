3ef8106a0652dd5956309041504e552ed978fabb
GitHub Action <action@github.com>
2022-07-22 09:36:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/maplepro/kotsuite/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/maplepro/kotsuite/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.maplepro.kotsuite.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.maplepro.kotsuite.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/maplepro/kotsuite/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/maplepro/kotsuite/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.maplepro.kotsuite.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.maplepro.kotsuite.MyBundle
 
 class MyApplicationService {
 
